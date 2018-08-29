4490523c214618e975cb9c41e12e6a76dfe55bfb
manoj khadilkar <sspm.manoj.khadilkar@gmail.com>
2018-08-29 09:52:43 +0530

    Initial commit

---
+++ Ch1.py
@@ -0,0 +1,20 @@
+#Data Operations in Numpy
+# more than one dimensions
+import numpy as np
+import pandas as pd
+a = np.array([[1, 2], [3, 4]])
+print (a)
+# minimum dimensions
+a = np.array([1, 2, 3,4,5], ndmin = 2)
+print (a)
+
+# dtype parameter
+a = np.array([1, 2, 3], dtype = complex)
+print (a)
+
+#Data Operations in Pandas
+#import the pandas library and aliasing as pd
+print("Series data")
+data = np.array(['a','b','c','d'])
+s = pd.Series(data)
+print(s)
